1751bb988c61eb76b281baed537e4be24e634d82
Christophe Tiraoui <krux@krux.fr>
2025-03-16 17:39:58 +0100

    ++

--- templates/2_physical/compute/plateforms/microk8s/ingress.tf
+++ templates/2_physical/compute/plateforms/microk8s/ingress.tf
@@ -11,12 +11,12 @@ resource "terraform_data" "microk8s_addons_ingress" {
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-        ADDON_ENABLED=$(microk8s.status --format short -a dns | grep enabled | wc -l)
+        ADDON_ENABLED=$(microk8s.status --format short -a ingress | grep enabled | wc -l)
         if [ $ADDON_ENABLED -eq 0 ]; then
-            microk8s.enable dns
+            microk8s.enable ingress
         fi
         mkdir -p $(pwd)/.k2/state/status
-        touch $(pwd)/.k2/state/status/microk8s.dns        
+        touch $(pwd)/.k2/state/status/microk8s.ingress        
     EOT
   }
 
@@ -24,11 +24,11 @@ resource "terraform_data" "microk8s_addons_ingress" {
     when        = destroy
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-        ADDON_ENABLED=$(microk8s.status --format short -a dns | grep enabled | wc -l)
+        ADDON_ENABLED=$(microk8s.status --format short -a ingress | grep enabled | wc -l)
         if [ $ADDON_ENABLED -eq 1 ]; then
-            microk8s.disable dns
+            microk8s.disable ingress
         fi
-        rm -rf $(pwd)/.k2/state/status/microk8s.dns
+        rm -rf $(pwd)/.k2/state/status/microk8s.ingress
     EOT
   }
 
