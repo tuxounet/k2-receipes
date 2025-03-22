ab7cc7dc4ba4fcdee58d27fb1f615cdd714c5be1
Christophe Tiraoui <krux@krux.fr>
2025-03-22 20:53:35 +0100

    ++

--- templates/2_physical/authority/standalone/main.tf
+++ templates/2_physical/authority/standalone/main.tf
@@ -3,17 +3,14 @@ resource "terraform_data" "authority_ca" {
     fileexists("${var.run_dir}/etc/pki/ca/ca.key"),
     fileexists("${var.run_dir}/etc/pki/ca/ca.crt"),
   ]
-  input = {
-    run_dir = var.run_dir
-
-  }
+ 
 
 
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
               
-        mkdir -p ${self.input.run_dir}/etc/pki/ca       
+        mkdir -p ${var.run_dir}/etc/pki/ca       
         if [ ! -f ${var.run_dir}/etc/pki/ca/ca.key ]; then
             openssl genrsa -out ${var.run_dir}/etc/pki/ca/ca.key 4096
         fi
@@ -21,18 +18,10 @@ resource "terraform_data" "authority_ca" {
         if [ ! -f ${var.run_dir}/etc/pki/ca/ca.crt ]; then
             openssl req -x509 -new -nodes -key ${var.run_dir}/etc/pki/ca/ca.key -sha256 -days 3650 -out ${var.run_dir}/etc/pki/ca/ca.crt -subj "/C=US/ST=CA/L=San Francisco/O=K2/OU=K2/CN=${var.ca_common_name}"
         fi
-
-    
+ 
     EOT
   }
-
-  provisioner "local-exec" {
-    when        = destroy
-    interpreter = ["/bin/bash", "-c"]
-    command     = <<EOT
-        rm -rf ${self.input.run_dir}/etc/pki/ca
-    EOT
-  }
+ 
 
 
 }
