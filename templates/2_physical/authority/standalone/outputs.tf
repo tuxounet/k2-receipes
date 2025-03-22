2232e4884ae9293a8a58c58a348cf94045c99f2c
Christophe Tiraoui <krux@krux.fr>
2025-03-22 22:54:38 +0100

    ++

--- templates/2_physical/authority/standalone/outputs.tf
+++ templates/2_physical/authority/standalone/outputs.tf
@@ -1,4 +1,7 @@
-output "k8s_config" {
-  value = abspath("../../.k2/home/.kube/config")
+output "root_ca_cert" {
+  value = "${var.run_dir}/etc/pki/ca/ca.crt"
 }
 
+output "root_ca_key" {
+  value = "${var.run_dir}/etc/pki/ca/ca.key"
+}
