e060e6be0b933bd4833b2dc8f58b64ed4bbe6c89
Christophe Tiraoui <krux@krux.fr>
2025-03-22 23:05:33 +0100

    ++

--- templates/2_physical/platforms/microk8s/main.tf
+++ templates/2_physical/platforms/microk8s/main.tf
@@ -4,27 +4,24 @@ resource "terraform_data" "microk8s_setup" {
     fileexists("${var.run_dir}/home/.kube/config"),
   ]
 
-   input = {
-    run_dir      = var.run_dir
-  
-  }
-
+  input = var.run_dir
 
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
+        RUN_DIR=${self.input}
         SNAP_PRESENT=$(snap list | grep "^microk8s" | wc -l)
         if [ $SNAP_PRESENT -eq 0 ]; then
             sudo snap install microk8s --classic            
-            mkdir -p ${self.input.run_dir}/state/status
-            touch ${self.input.run_dir}/state/status/microk8s
+            mkdir -p $RUN_DIR/state/status
+            touch $RUN_DIR/state/status/microk8s
             sudo usermod -a -G microk8s $(whoami)        
             newgrp microk8s
             sleep 15
         fi
         microk8s status --wait-ready        
-        mkdir -p ${self.input.run_dir}/home/.kube
-        microk8s config > ${self.input.run_dir}/home/.kube/config        
+        mkdir -p $RUN_DIR/home/.kube
+        microk8s config > $RUN_DIR/home/.kube/config        
         
     EOT
   }
@@ -33,13 +30,14 @@ resource "terraform_data" "microk8s_setup" {
     when        = destroy
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
+        RUN_DIR=${self.input}
         SNAP_PRESENT=$(snap list | grep "^microk8s" | wc -l)
         if [ $SNAP_PRESENT -eq 1 ]; then
             sudo snap remove microk8s   
-            rm -rf ${self.input.run_dir}/state/status/microk8s
+            rm -rf $RUN_DIR/state/status/microk8s
         fi
+        rm -rf $RUN_DIR/home/.kube
 
-        rm -rf ${self.input.run_dir}/home/.kube
         
     EOT
   }
--- templates/2_physical/platforms/microk8s/storage.tf
+++ templates/2_physical/platforms/microk8s/storage.tf
@@ -10,26 +10,27 @@ module "k2_enable_microk8s_storage" {
 }
 
 
-# resource "kubernetes_manifest" "local_storage_class" {
-#   depends_on = [
-#     terraform_data.microk8s_setup, 
-#   ]
-#   manifest = {
-#     apiVersion = "storage.k8s.io/v1"
-#     kind       = "StorageClass"
-#     metadata = {
-#       name = "local-storage"
-#     }
-#     provisioner = "microk8s.io/hostpath"
-#     parameters = {
-#       pvDir = "${var.run_dir}/.k2/home/data"
+resource "kubernetes_manifest" "local_storage_class" {
+  depends_on = [
+    terraform_data.microk8s_setup,
+    module.k2_enable_microk8s_storage
+  ]
+  manifest = {
+    apiVersion = "storage.k8s.io/v1"
+    kind       = "StorageClass"
+    metadata = {
+      name = "local-storage"
+    }
+    provisioner = "microk8s.io/hostpath"
+    parameters = {
+      pvDir = "${var.run_dir}/home/data"
 
-#     }
-#     volumeBindingMode = "WaitForFirstConsumer"
-#   }
-#   timeouts {
-#     create = "1m"
-#     update = "1m"
-#     delete = "30s"
-#   }
-# }
+    }
+    volumeBindingMode = "WaitForFirstConsumer"
+  }
+  timeouts {
+    create = "1m"
+    update = "1m"
+    delete = "30s"
+  }
+}
