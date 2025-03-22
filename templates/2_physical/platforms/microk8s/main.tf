af9cc206dd6bc645d44d3b1976bad6be0d25bf4a
Christophe Tiraoui <krux@krux.fr>
2025-03-22 17:14:58 +0100

    ++

--- templates/2_physical/platforms/microk8s/main.tf
+++ templates/2_physical/platforms/microk8s/main.tf
@@ -15,7 +15,7 @@ resource "terraform_data" "microk8s_setup" {
             touch $(pwd)/.k2/state/status/microk8s
             sudo usermod -a -G microk8s $(whoami)        
             newgrp microk8s
-          
+            sleep 15
         fi
         microk8s status --wait-ready        
         mkdir -p $(pwd)/.k2/home/.kube
