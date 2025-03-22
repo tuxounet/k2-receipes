f00a4a121be6e0400b87397bcded6c9087d230da
Christophe Tiraoui <krux@krux.fr>
2025-03-22 19:57:43 +0100

    ++

--- templates/1_provisional/gcs/provisionners/terraform.auth.sh
+++ templates/1_provisional/gcs/provisionners/terraform.auth.sh
@@ -12,6 +12,6 @@ fi
 ROOT_DIR=$(git rev-parse --show-toplevel)
 RUN_DIR=${ROOT_DIR}/.k2
 
-WORKSPACE_NAME:=$(shell git rev-parse --abbrev-ref HEAD || echo "sample")
+WORKSPACE_NAME:=$(git rev-parse --abbrev-ref HEAD || echo "sample")
 mkdir -p ${RUN_DIR}/state/${WORKSPACE_NAME}
 
