3e74d81df240e6c65f33f61911f0078c8c505865
Christophe Tiraoui <krux@krux.fr>
2025-03-22 22:44:11 +0100

    ++

---
+++ templates/3_technical/management/iam/ldap_provisioning.tf
@@ -0,0 +1,13 @@
+provider "ldap" {
+
+  host          = "localhost"
+  port          = 31389
+  bind_user     = "cn=admin,${var.ldap_base_dn}"
+  bind_password = random_password.ldap_root_password.result
+}
+
+resource "ldap_ou" "org" {
+  name        = var.ldap_org
+  ou          = "ou=${var.ldap_org},${var.ldap_base_dn}"
+  description = "My OU description"
+}
--- templates/3_technical/management/iam/providers.tf
+++ templates/3_technical/management/iam/providers.tf
@@ -14,9 +14,3 @@ provider "kubernetes" {
 
 }
 
-provider "ldap" {
-  host          = "ldap.mycompany.tld"
-  port          = 389
-  bind_user     = "ldap_user"
-  bind_password = "ldap_password"
-}
